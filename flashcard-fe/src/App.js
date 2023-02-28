2a9d667f97b73bce14a79967709825c6f3b99678
sushruthravishankar <sravishankar@hillhouse.doncaster.sch.uk>
2023-02-28 22:54:00 +0000

    react added + flashcards rendered as basic table (created screenshot)

---
+++ flashcard-fe/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from "react";
+import Header from "./components/Header";
+import Home from "./components/Home";
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Home />
+      </Fragment>
+    );
+  }
+}
+
+export default App;
